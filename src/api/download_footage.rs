55394b47ef36be4b2d06e95a8248fbbf509ec336
Lars Frölich <lars.froelich@gmail.com>
2023-07-25 15:11:19 +0200

    attempting multiple camera channels during a download

--- src/api/download_footage.rs
+++ src/api/download_footage.rs
@@ -12,42 +12,45 @@ impl UnifiProtectServer {
         start_unix: i64,
         end_unix: i64,
     ) -> Result<bool, String> {
-        let endpoint = format!(
-            "{}/proxy/protect/api/video/export?camera={}\
-            &channel=0\
-            &filename={}.mp4\
-            &lens=0\
-            &start={}\
-            &end={}\
-            &type={}",
-            self.uri, camera.id, camera.mac, start_unix, end_unix, recording_type
-        );
+        for i in 0..4 {
+            let endpoint = format!(
+                "{}/proxy/protect/api/video/export?camera={}\
+                &channel={}\
+                &filename={}.mp4\
+                &lens=0\
+                &start={}\
+                &end={}\
+                &type={}",
+                self.uri, camera.id, i, camera.mac, start_unix, end_unix, recording_type
+            );
 
-        let response = Client::builder()
-            .danger_accept_invalid_certs(true)
-            .build()
-            .unwrap()
-            .get(&endpoint)
-            .headers(self.headers.clone())
-            .send()
-            .await
-            .expect("Failed to send request");
-        if !response.status().is_success() {
-            if response
-                .json::<ErrorResponse>()
+            let response = Client::builder()
+                .danger_accept_invalid_certs(true)
+                .build()
+                .unwrap()
+                .get(&endpoint)
+                .headers(self.headers.clone())
+                .send()
                 .await
-                .expect("Failed to parse response json")
-                .error
-                .contains("o files found")
-            {
-                return Ok(false);
+                .expect("Failed to send request");
+            if !response.status().is_success() {
+                let error_msg = response
+                    .json::<ErrorResponse>()
+                    .await
+                    .expect("Failed to parse response json")
+                    .error;
+                if error_msg
+                    .contains("o files found") || error_msg.contains("track information is not valid"){
+                    continue;
+                }
+                return Err(format!("Failed to download video."));
             }
-            return Err(format!("Failed to download video."));
-        }
-        let mut file = std::fs::File::create(output_path).expect("Failed to create file");
-        let mut content = Cursor::new(response.bytes().await.expect("Failed to get bytes"));
-        std::io::copy(&mut content, &mut file).expect("Failed to copy content to file");
+            let mut file = std::fs::File::create(output_path).expect("Failed to create file");
+            let mut content = Cursor::new(response.bytes().await.expect("Failed to get bytes"));
+            std::io::copy(&mut content, &mut file).expect("Failed to copy content to file");
 
-        Ok(true)
+            return Ok(true)
+        }
+        return Ok(false)
     }
 }

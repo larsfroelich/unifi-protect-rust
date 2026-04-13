e0c59ea1e657649980dd8fad7d9db960ed648eec
Lars Frölich <lars.froelich@gmail.com>
2026-04-13 14:57:07 +0200

    download video in chunks (streaming to file) to keep memory usage low

--- src/api/download_footage.rs
+++ src/api/download_footage.rs
@@ -1,7 +1,7 @@
-use crate::camera::{UnifiProtectCameraSimple};
+use crate::camera::UnifiProtectCameraSimple;
 use crate::{ErrorResponse, UnifiProtectServer};
 use reqwest::Client;
-use std::io::Cursor;
+use tokio::io::AsyncWriteExt;
 
 impl UnifiProtectServer {
     pub async fn download_footage(
@@ -22,16 +22,21 @@ impl UnifiProtectServer {
                 &start={}\
                 &end={}\
                 &type={}",
-                self.uri, camera.id,
+                self.uri,
+                camera.id,
                 (if recording_type == "timelapse" {
                     "&fps=4"
                 } else {
                     ""
                 }),
-                channel, camera.mac, start_unix, end_unix, recording_type
+                channel,
+                camera.mac,
+                start_unix,
+                end_unix,
+                recording_type
             );
 
-            let response = Client::builder()
+            let mut response = Client::builder()
                 .danger_accept_invalid_certs(true)
                 .build()
                 .unwrap()
@@ -40,32 +45,41 @@ impl UnifiProtectServer {
                 .send()
                 .await
                 .expect("Failed to send request");
+
             if !response.status().is_success() {
                 eprintln!("Error: {:?}", response);
                 let status_code = response.status();
-                let error_msg = response
-                    .json::<ErrorResponse>()
-                    .await
-                    .ok()
-                    .map(|x|x.error);
-                if error_msg.is_some() && (error_msg.as_ref().unwrap()
-                    .contains("o files found") || error_msg.as_ref().unwrap().contains("track information is not valid")){
+                let error_msg = response.json::<ErrorResponse>().await.ok().map(|x| x.error);
+                if error_msg.is_some()
+                    && (error_msg.as_ref().unwrap().contains("o files found")
+                        || error_msg
+                            .as_ref()
+                            .unwrap()
+                            .contains("track information is not valid"))
+                {
                     continue;
-                }else{
+                } else {
                     if error_msg.is_some() {
                         eprintln!("Error: {}", error_msg.unwrap());
-                    }else {
+                    } else {
                         eprintln!("Unknown Error - Status Code: {}", status_code);
                     }
                     eprintln!("Failed to download video.");
                     continue;
                 }
             }
-            let mut file = std::fs::File::create(output_path).expect("Failed to create file");
-            let mut content = Cursor::new(response.bytes().await.expect("Failed to get bytes"));
-            std::io::copy(&mut content, &mut file).expect("Failed to copy content to file");
 
-            return Ok(true)
+            let mut file = tokio::fs::File::create(output_path)
+                .await
+                .expect("Failed to create file");
+
+            while let Some(chunk) = response.chunk().await.expect("Failed to read response chunk") {
+                file.write_all(&chunk)
+                    .await
+                    .expect("Failed to write video chunk to file");
+            }
+
+            return Ok(true);
         }
 
         Ok(false)
